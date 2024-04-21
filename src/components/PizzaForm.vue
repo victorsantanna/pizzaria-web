b8ad70abbe28faf5f670bb19c6b98f444858573b
victorsantanna <victor.esef@hotmail.com>
2024-04-21 20:58:39 -0300

    feat: adição das reatividade do vue

--- src/components/PizzaForm.vue
+++ src/components/PizzaForm.vue
@@ -1,42 +1,43 @@
 <template>
     <div>
-        <p> C. de mensagem</p>
-    </div>
-    <div>
-        <form action="" id="pizza-form">
-            <div class="input-container">
-                <label for="nome">Nome do Cliente</label>
-                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
-            </div>
-            <div class="input-container">
-                <label for="Pizza">Escolha a Massa:</label>
-                <select name="pizza" id="pizza" v-model="pizza">
-                <option value="">Selecione o tipo da massa</option>
-                <option v-for="pizza in pizzas" :key="pizza.id" :value="pizza.tipo">{{ pizza.tipo }}</option>
-                </select>
-            </div>
-            <div class="input-container">
-                <label for="recheio">Escolha o recheio:</label>
-                <select name="recheio" id="recheio" v-model="recheio">
-                <option value="">Selecione o tipo do recheio</option>
-                <option v-for="recheio in recheios" :key="recheio.id" :value="recheio.tipo">{{ recheio.tipo }}</option>
-                </select>
-            </div>
-            <div id="opcionais-container" class="input-container">
-                <label for="opcionais" id="opcionais-title">Escolha os opcionais:</label>
-                <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
-                    <span>{{ opcional.tipo }}</span>
+        <Message :msg="msg" v-show="msg"/>
+        <div>
+            <form action="" id="pizza-form" @submit="createPizza">
+                <div class="input-container">
+                    <label for="nome">Nome do Cliente</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
                 </div>
-            </div>
-            <div class="input-container">
-                <input type="submit" class="submit-btn" value="Criar minha Pizza">
-            </div>
-        </form>
+                <div class="input-container">
+                    <label for="Pizza">Escolha a Massa:</label>
+                    <select name="pizza" id="pizza" v-model="pizza">
+                    <option value="">Selecione o tipo da massa</option>
+                    <option v-for="pizza in pizzas" :key="pizza.id" :value="pizza.tipo">{{ pizza.tipo }}</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="recheio">Escolha o recheio:</label>
+                    <select name="recheio" id="recheio" v-model="recheio">
+                    <option value="">Selecione o tipo do recheio</option>
+                    <option v-for="recheio in recheios" :key="recheio.id" :value="recheio.tipo">{{ recheio.tipo }}</option>
+                    </select>
+                </div>
+                <div id="opcionais-container" class="input-container">
+                    <label for="opcionais" id="opcionais-title">Escolha os opcionais:</label>
+                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar minha Pizza">
+                </div>
+            </form>
+    </div>
     </div>
 </template>
 
 <script>
+import Message from './Message.vue';
 export default {
     name:"PizzaForm",
     data(){
@@ -46,9 +47,8 @@ export default {
             opcionaisdata: null,
             nome: null,
             pizza:null,
-            recheios: null,
+            recheio: null,
             opcionais: [],
-            status:"Solicitado",
             msg: null, 
         }
 
@@ -63,10 +63,46 @@ export default {
             this.recheios = data.recheios;
             this.opcionaisdata = data.opcionais;
 
+        },
+        async createPizza(e){
+
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                recheio: this.recheio,
+                pizza: this.pizza,
+                opcionais: Array.from(this.opcionais),
+                status: "solicitado",
+            }
+
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/pizzas", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            
+        });
+        const res = await req.json();
+
+        this.msg = `Pedido Nº ${res.id} realizado com sucesso`;
+        setTimeout(() => this.msg ="", 3000)
+
+        this.nome = "";
+        this.recheio = "";
+        this.pizza = "";
+        this.opcionais = "";
+        
+
+
         }
     },
     mounted(){
         this.getIngredientes()
+    },
+    components:{
+        Message
     }
     
 }
