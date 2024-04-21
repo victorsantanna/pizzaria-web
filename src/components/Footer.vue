00cc8cc3e27c970042e71ab464777cd5d01da0ae
victorsantanna <victor.esef@hotmail.com>
2024-04-21 14:02:06 -0300

    feat: adição das funcionalidades do navbar e footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Footer</p>
+</template>
+
+<script>
+export default {
+    name:"Footer"
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            <img src="" alt="" id="logo">
+        </router-link>
+        <router-link to="/">HOME</router-link> |
+        <router-link to="/pedidos">PEDIDOS</router-link>
+
+    </div>
+  
+  
+    
+</template>
+
+<script>
+export default {
+    name:"Navbar",
+    
+}
+</script>
+
+<style scoped>
+
+</style>
