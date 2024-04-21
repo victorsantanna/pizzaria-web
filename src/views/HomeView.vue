ed68f4f05d548a7b248f34baf5ebd078efe4dada
victorsantanna <victor.esef@hotmail.com>
2024-04-21 15:37:46 -0300

    feat: importação do componente

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu Pizza:</h1>
-    <p>Formulário</p>
+    <PizzaForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import PizzaForm from "../components/PizzaForm.vue"
 
 export default{
   name:"Home",
   components:{
     Banner,
+    PizzaForm,
   }
 }
 
