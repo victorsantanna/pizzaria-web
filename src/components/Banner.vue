94440a7ae9bf76bacfefae79497d60abf6ca508a
victorsantanna <victor.esef@hotmail.com>
2024-04-21 14:54:49 -0300

    style: adição da estilzação do banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,38 @@
+<template>
+   <div id="main-banner">
+    <h1> Make Your Pizza</h1>
+   </div>
+</template>
+
+<script>
+export default {
+    name:"Banner"
+    
+}
+</script>
+
+<style scoped>
+#main-banner{
+    background-image: url('/public/img/pizza.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1{
+    color:#FCBA03;
+    text-align: center;
+    font-size: 60px;
+    font-family: Tahoma, sans-serif;
+    background-color: #222;
+    padding: 20px 40px;
+    opacity: 0.8;   
+    border-radius: 0 30px 30px 0; 
+
+}
+
+
+</style>
