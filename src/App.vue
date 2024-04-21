2e822c0ebddc9fc63056b6a5074b9c509330a428
victorsantanna <victor.esef@hotmail.com>
2024-04-21 14:28:21 -0300

    feat: adição do data e estilização do App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar />
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer />
   
@@ -13,11 +13,35 @@ export default {
   components:{
     Navbar,
     Footer,
+  },
+  data(){
+   return {
+    logo_src: "/img/logo.png",
+    app_name: "Make Your Pizza"
+   } 
   }
   
 }
 </script>
 
 <style>
+*{
+  font-family:Tahoma, sans-serif ;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container{
+  margin: 50px;
+  min-height: 250px;
+  
+}
+
+h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 
 </style>
