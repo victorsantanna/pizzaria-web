595a7488380da2063456a5e5b19615206f0f2d52
victorsantanna <victor.esef@hotmail.com>
2024-04-21 14:29:44 -0300

    feat: adição das props
    style: adição da estilização

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="nav">
-        <router-link to="/">
-            <img src="" alt="" id="logo">
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
         </router-link>
-        <router-link to="/">HOME</router-link> |
+        <router-link to="/">HOME</router-link> 
         <router-link to="/pedidos">PEDIDOS</router-link>
 
     </div>
@@ -15,10 +15,39 @@
 <script>
 export default {
     name:"Navbar",
+    props:["logo", "alt"],
     
 }
 </script>
 
 <style scoped>
+#nav{
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
 
+#nav #logo-url{
+    margin: auto;
+    margin-left: 0;
+}
+#logo{
+    width: 100px;
+    height: 100px;
+}
+
+#nav a{
+    color: #FCBA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .3s;
+
+}
+
+#nav a:hover{
+    color:#fff
+}
 </style>
