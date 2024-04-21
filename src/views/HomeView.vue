c1a6bdd83475538b98c44d08da57feb1742189f9
victorsantanna <victor.esef@hotmail.com>
2024-04-21 14:55:18 -0300

    feat: adição do import e das tags h1 e p

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu Pizza:</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
+
+export default{
+  name:"Home",
+  components:{
+    Banner,
+  }
+}
 
 </script>
